b49541c9c04c319bb062c1a1edc6a3fa11b72eb5
shivamkarn <shivamkarn767@gmail.com>
2026-01-18 13:05:33 +0545

    Initial setup with ShadCN components

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = {
+  title: "ExpenseTracker",
+  description: "The place to track expenses",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>
+        {/* header */}
+        <main>{children}</main>
+        {/* Footer */}
+        <footer className="bg-blue-100 py-12">
+          <div className="container  mx-auto px-4 text-center tex-gray-600">
+            <p>Made By Shivam Karn</p>
+          </div>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <Button variant={"destructive"}>
+      This is shivam karn expense tracker project initialization
+    </Button>
+  );
+}
