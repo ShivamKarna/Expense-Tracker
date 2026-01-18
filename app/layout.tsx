810224a7fece2989090551a408c89d865006df5f
shivamkarn <shivamkarn767@gmail.com>
2026-01-18 15:16:11 +0545

    Added clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -14,17 +16,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        {/* header */}
-        <main>{children}</main>
-        {/* Footer */}
-        <footer className="bg-blue-100 py-12">
-          <div className="container  mx-auto px-4 text-center tex-gray-600">
-            <p>Made By Shivam Karn</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          {/* header */}
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          {/* Footer */}
+          <footer className="bg-blue-100 py-12">
+            <div className="container  mx-auto px-4 text-center tex-gray-600">
+              <p>Made By Shivam Karn</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
