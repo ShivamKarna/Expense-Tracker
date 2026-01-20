ad3f61dfb2463c0a6daa8775c99dea793e7093c1
shivamkarn <shivamkarn767@gmail.com>
2026-01-20 21:25:59 +0545

    Add useFetch hook for simplified data fetching with loading and error handling

---
+++ hooks/use-fetch.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { toast } from "sonner";
+
+const useFetch = <T, Args extends unknown[] = []>(
+  cb: (...args: Args) => Promise<T>,
+) => {
+  const [data, setData] = useState<T | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | string | null>(null);
+
+  const fn = async (...args: Args) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await cb(...args);
+      setData(response);
+      setError(null);
+    } catch (error) {
+      setError(error instanceof Error ? error : "An Error occured");
+      toast.error(error instanceof Error ? error.message : String(error));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, fn };
+};
+
+export default useFetch;
